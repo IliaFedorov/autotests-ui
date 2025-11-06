f46dc20acabfc887938fc9188216ca9f7490de2e
IliaFedorov <fedilvy88@gmail.com>
2025-11-06 22:42:26 +0300

    5.6 Работа с авторизацией в Playwright Практика работы с авторизацией в Playwright

--- playwright_courses.py
+++ playwright_courses.py
@@ -8,7 +8,7 @@ with sync_playwright() as playwright:
 
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-# Registering a new User:
+    # Registering a new User:
     registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
     registration_email_input.fill("user.name@gmail.com")
 
@@ -31,18 +31,21 @@ with sync_playwright() as playwright:
 
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
+    #Checking "Courses" header
     courses_list_toolbar_title = page.get_by_test_id('courses-list-toolbar-title-text')
     expect(courses_list_toolbar_title).to_be_visible()
     expect(courses_list_toolbar_title).to_have_text("Courses")
 
+    #Checking list view text
     courses_list_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
     expect(courses_list_view_title).to_be_visible()
     expect(courses_list_view_title).to_have_text("There is no results")
 
+    # Checking list icon
     courses_list_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
     expect(courses_list_view_icon).to_be_visible()
 
+    # Checking list description
     courses_list_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
     expect(courses_list_view_description).to_be_visible()
     expect(courses_list_view_description).to_have_text("Results from the load test pipeline will be displayed here")
-
