2f394aef117b35d7494f782982d2fcf4b424b4a9
IliaFedorov <fedilvy88@gmail.com>
2025-11-07 22:57:03 +0300

    6.1 Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,16 @@
+def test_user_login():
+    print("-----Hola-----")
+
+class TestUserLogin:
+
+    def test_1(self):
+        print('-=-=1-=-=')
+
+    def test_2(self):
+        print('-=-=2-=-=')
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5, print("FAILED MISERABLY")
