fc145044c8ecbdef171b6873c7d1040c5c46b6f1
IliaFedorov <fedilvy88@gmail.com>
2025-11-09 17:49:38 +0300

    6.4 Pytest skip, skipif, xfail

---
+++ tests/test_pytest_skip.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.mark.skip(reason="in development")
+def test_feature_in_development():
+    ...
+
+@pytest.mark.skip(reason="in development")
+class TestSuiteSkip:
+
+    def test_feature_in_development_1(self):
+        ...
+
+    def test_feature_in_development_2(self):
+        ...
+
+
---
+++ tests/test_pytest_xfail.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.xfail(reason="Найден баг в приложении, из-за которого тест падает с ошибкой")
+def test_with_bug():
+    assert 1 == 2
+
+@pytest.mark.xfail(reason="Баг исправлен, но на тест еще висит маркировка")
+def test_without_bug():
+    ...
