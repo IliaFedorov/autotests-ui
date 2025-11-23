cf579199854f88305f75295b774c87ee2436bcaf
IliaFedorov <fedilvy88@gmail.com>
2025-11-23 03:57:56 +0300

    7.7 Наращивание тестовой базы и рефакторинг Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -20,25 +20,19 @@ class TestCourses:
         create_course_page.create_course_form.check_visible(
             title="Playwright2", estimated_time="4 weeks", description="Playwright2", max_score="111", min_score="11"
         )
-
         create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
         create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
-
         create_course_page.create_course_toolbar.click_create_course_button()
-
 
         courses_list_page.course_view.check_visible(
             index=0, title="Playwright2", estimated_time="4 weeks", max_score="111", min_score="11"
         )
 
         courses_list_page.course_view.menu.click_edit(index=0)
-
-
         create_course_page.create_course_form.fill(
             title="Playwright3", estimated_time="3 weeks", description="Playwright3", max_score="333", min_score="33"
         )
         create_course_page.create_course_toolbar.click_create_course_button()
-
 
         courses_list_page.course_view.check_visible(
             index=0, title="Playwright3", estimated_time="3 weeks", max_score="333", min_score="33"
