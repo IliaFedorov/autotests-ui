b0a5079d3029e02ae50b97bf0882a5c643cd1ba4
IliaFedorov <fedilvy88@gmail.com>
2025-11-27 20:51:44 +0300

    9.5 Логирование взаимодействий со страницей Практика работы с логированием

--- components/base_component.py
+++ components/base_component.py
@@ -1,10 +1,13 @@
 from playwright.sync_api import Page, expect
 from typing import Pattern
+from tools.logger import get_logger
 
+logger = get_logger("CHECK_URL")
 
 class BaseComponent:
     def __init__(self, page: Page):
         self.page = page
 
     def check_current_url(self, expected_url: Pattern[str]):
+        logger.info(f"Checking current url: {expected_url.pattern}")
         expect(self.page).to_have_url(expected_url)
