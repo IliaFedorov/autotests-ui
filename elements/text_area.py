eae2ca724befac4c655b8e03dac3455bcb0785ec
IliaFedorov <fedilvy88@gmail.com>
2025-12-09 14:45:52 +0300

    11.3 практикуемся в интеграции покрытия в UI тесты практическое задание: абота с инструментом ui-coverage-tool

--- elements/text_area.py
+++ elements/text_area.py
@@ -17,7 +17,7 @@ class TextAreaInput(BaseElement):
             return super().get_locator(nth, **kwargs).locator('textarea').first
 
     def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
-        return f"//*[@data-testid='{self.locator.format(**kwargs)}'].first"
+        return f"//*[@data-testid='{self.locator.format(**kwargs)}'][{nth + 1}]"
 
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
